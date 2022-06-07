909a5241f6ec491674dd7ff610dbe0d52ac7ccf1
Nehuen Llanos <nehuengllanos@gmail.com>
2022-06-07 19:45:05 -0300

    Prueba cambios en los nombres

--- Unidad_I_Algoritmos/AnalysisTime/src/main/java/Proof.java
+++
@@ -1,7 +0,0 @@
-public class Proof {
-    public static void main(String[] args) {
-        MyTimer myCrono = new MyTimer(10);
-        myCrono.stop(10 + 93623040);
-        System.out.println(myCrono);
-    }
-}
